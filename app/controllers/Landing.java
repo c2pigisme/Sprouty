d797f497db2ae533ee28bf5e718191720f1d4755
kokboon <ktan@atex.com>
2012-04-05 15:16:48 +0800

    Initial checkin for sprouty, it only consists of drafted PoC code to prove that foursquare and google map are supports for location based feature

---
+++ app/controllers/Landing.java
@@ -0,0 +1,14 @@
+package controllers;
+
+import play.mvc.Controller;
+
+public class Landing extends Controller {
+	
+	public static void index() {
+		render();
+	}
+
+	public static void test() {
+		render();
+	}
+}
